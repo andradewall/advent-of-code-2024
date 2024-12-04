3b19f8cb3fd88122a6fe7b0ea7f84f5153578fed
Wallace A <andradewall0@gmail.com>
2024-12-04 19:33:56 -0300

    feat(day01): add part two of the challenge and format the file

--- src/days/day01.php
+++ src/days/day01.php
@@ -2,26 +2,50 @@
 
 declare(strict_types=1);
 
-// 1. READ INPUT FROM FILE
 $file = file(__DIR__ . '/../../assets/inputs/days/day01.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-// 2. SPLIT THE INPUT TO LEFT AND RIGHT LIST
 $leftList = [];
 $rightList = [];
 foreach ($file as $line) {
-    [$leftList[], $rightList[]] = explode("  ", $line);
+    [$leftList[], $rightList[]] = explode("   ", $line);
 }
 
-// 3. SORT BOTH LIST BY ASC
-sort($leftList);
-sort($rightList);
+printf("Result for part one: " . partOne($leftList, $rightList));
+printf("\nResult for part two: " . partTwo($leftList, $rightList));
 
-// 4. GET THE DIFF BETWEEN ALL INDEXES AND SUM IT
-$sum = 0;
-for ($i = 0; $i < count($file); $i++) {
-    $diff = abs((int) $leftList[$i] - (int) $rightList[$i]);
-    $sum += $diff;
+/**
+ * @param array<int, string>
+ * @param array<int, string>
+ * @return int
+ */
+function partOne(array $leftList, array $rightList): int
+{
+    sort($leftList);
+    sort($rightList);
+
+    $sum = 0;
+    for ($i = 0; $i < count($leftList); $i++) {
+        $diff = abs((int) $leftList[$i] - (int) $rightList[$i]);
+        $sum += $diff;
+    }
+
+    return $sum;
 }
 
-// 5. DISPLAY THE SUM
-echo "Result: {$sum}";
+/**
+ * @param array<int, string>
+ * @param array<int, string>
+ * @return int
+ */
+function partTwo(array $leftList, array $rightList): int
+{
+    $locationAppearsCount = 0;
+    $similarityScore = 0;
+    foreach ($leftList as $location) {
+        $locationAppearsCount = count(array_filter($rightList, fn ($i) => $i === $location));
+
+        $similarityScore += (int) $location * (int) $locationAppearsCount;
+    }
+
+    return $similarityScore;
+}
